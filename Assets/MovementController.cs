f76e1e1740fc5413a7971a8594db48b5310b96d7
Ryan Gill <ryan.gill8120@gmail.com>
2022-09-29 21:07:53 -0500

    Got chad moving

--- Assets/MovementController.cs
+++ Assets/MovementController.cs
@@ -5,17 +5,48 @@ using UnityEngine;
 public class MovementController : MonoBehaviour
 {
     // Start is called before the first frame update
+
+
+    
+    Vector3 clickPosition;
+
+
+    [SerializeField]
+    GameObject ChadModel;
+
     void Start()
     {
+        
+        clickPosition = transform.position;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 clickPosition = -Vector3.one;
+          
+            Plane plane = new Plane(Vector3.up, 0);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float distanceToPlane;
+
+            if (plane.Raycast(ray, out distanceToPlane))
+            {
+                var rayPosition = ray.GetPoint(distanceToPlane);
+                rayPosition.y = 0f;
+                clickPosition = rayPosition;
+            }
+                
+
         }
+    }
+
+    private void FixedUpdate()
+    {
+        ChadModel.transform.LookAt(clickPosition);
+        transform.position = Vector3.MoveTowards(transform.position, clickPosition, 5 * Time.deltaTime);
+        
     }
 }
